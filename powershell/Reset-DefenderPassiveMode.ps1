2740ebf55e5ad23a9b3490705781e78fb3e6bc8e
gromedev <61883549+gromedev@users.noreply.github.com>
2022-10-27 15:08:11 +0200

    Create Reset-DefenderPassiveMode.ps1

---
+++ powershell/Reset-DefenderPassiveMode.ps1
@@ -0,0 +1,105 @@
+<#
+.SYNOPSIS
+    Remediates Defender passive mode
+.AUTHOR
+    thomas@grome.dev
+#>
+
+[cmdletbinding()]
+param(
+    $IncomingValue,
+    $Remediate = $False
+)
+
+
+$NoncompliantCount = 0
+
+$PropertyList = @{}
+$PassiveMode =
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender"
+        Property = "PassiveMode"
+    }
+
+try {
+    $PassiveModeCurrentValue = Get-ItemPropertyValue -Path registry::"$($PassiveMode.Key)" -Name $PassiveMode.Property -ErrorAction SilentlyContinue
+}
+catch {
+}
+
+$PropertyList = @(
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WdBoot"
+        Property = "Group"
+        PropertyType = "String"
+        Value = "Early-Launch"
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WdBoot"
+        Property = "Start"
+        PropertyType = "DWord"
+        Value = 0
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WdFilter"
+        Property = "Start"
+        PropertyType = "DWord"
+        Value = 0
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender"
+        Property = "DisableAntiSpyware"
+        PropertyType = "DWord"
+        Value = 0
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender"
+        Property = "DisableAntiVirus"
+        PropertyType = "DWord"
+        Value = 0
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender"
+        Property = "PassiveMode"
+        PropertyType = "DWord"
+        Value = if($PassiveModeCurrentValue -eq 0) { 0 } else { 2 }
+    },
+
+    [PSCustomObject]@{
+        Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WinDefend"
+        Property = "Start"
+        PropertyType = "DWord"
+        Value = 2
+    }
+)
+
+foreach($PropertyItem in $PropertyList) {
+    try {
+        $CurrentValue = Get-ItemPropertyValue -Path registry::"$($PropertyItem.Key)" -Name $PropertyItem.Property -ErrorAction SilentlyContinue
+    }
+    catch {
+    }
+
+    if($CurrentValue -ne $PropertyItem.Value) {
+    $NoncompliantCount++
+        try {
+            if($Remediate) {
+                New-ItemProperty -Path registry::"$($PropertyItem.Key)"  -Name $PropertyItem.Property -PropertyType $PropertyItem.ValueType -Value $PropertyItem.Value -Force -ErrorAction Stop | Out-Null
+            }
+        }
+        catch {
+            Write-Host "Error Setting New Value"
+            $PropertyItem
+            $CurrentValue
+        }
+    }
+
+    $CurrentValue = $null
+}
+
+return $NoncompliantCount
