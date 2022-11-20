2821b66657707f10117831cf1d5b2a0f0ea0d856
gromedev <61883549+gromedev@users.noreply.github.com>
2022-11-20 23:02:55 +0100

    Add files via upload

---
+++ powershell/WindowsHelloPasswordless-Detection.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.SYNOPSIS
+    Detection script: checks if:
+        BitLocker is enabled.
+        Windows Hello is enabled.
+
+.REQUIREMENTS
+    Run this script using the logged-on credentials: No
+    Enforce script signature check: No
+    Run script in 64-bit PowerShell: Yes
+#>
+
+$BitLockerStatus = Get-Bitlockervolume
+
+
+if($BitLockerStatus.ProtectionStatus -eq 'On' -and $BitLockerStatus.EncryptionPercentage -eq '100'){
+    if((Test-Path $registryPath)) {
+        if(Get-ItemProperty -Path $registryPath -Name $registryValueName -ErrorAction Ignore) {
+            if((Get-ItemPropertyValue -Path $registryPath -Name $registryValueName -ErrorAction Ignore)-eq $registryValueData) {
+                Write-Host "Windows Hello for Business is required"
+                exit 0
+            }
+            else {
+                Write-Host "Windows Hello for Business is currently not required"
+                exit 1 
+            }
+        }
+        else {
+            Write-Host "Windows Hello for Business is currently not required"
+            exit 1 
+        }
+    }
+    else {
+        Write-Host "Windows Hello for Business is currently not required"
+        exit 1
+    } 
+}
+
+
+
---
+++ powershell/WindowsHelloPasswordless-Remediation.ps1
@@ -0,0 +1,30 @@
+﻿<#
+.SYNOPSIS
+    Remediation script: if detection script does NOT find Windows Hello is active; then configures Windows Hello passwordless mode. 
+
+.REQUIREMENTS
+    Run this script using the logged-on credentials: No
+    Enforce script signature check: No
+    Run script in 64-bit PowerShell: Yes
+#>
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$registryValueName = "scforceoption"
+$registryValueData = "1"
+
+try {
+    if(!(Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force
+        New-ItemProperty -Path $registryPath -Name $registryValueName -Value $registryValueData -PropertyType DWORD -Force
+        Write-Host "Successfully configured Windows Hello for Business as required" 
+    }
+    else {
+        New-ItemProperty -Path $registryPath -Name $registryValueName -Value $registryValueData -PropertyType DWORD -Force
+        Write-Host "Successfully configured Windows Hello for Business as required" 
+    }
+}
+catch {
+    $errorMessage = $_.Exception.Message
+    Write-Error $errorMessage
+    exit 1 
+}
