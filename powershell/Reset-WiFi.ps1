599a2ee94541d1b03777b06cfb2db436942b2327
gromedev <61883549+gromedev@users.noreply.github.com>
2024-01-15 15:33:03 +0100

    Add files via upload

---
+++ powershell/Reset-WiFi.ps1
@@ -0,0 +1,14 @@
+﻿netsh winsock reset
+netsh int ip reset
+ipconfig /renew
+ipconfig /flushdns
+netcfg -d
+Get-PnpDevice -class net | ? Status -eq Unknown | Select FriendlyName,InstanceId
+$Devs = Get-PnpDevice -class net | ? Status -eq Unknown | Select FriendlyName,InstanceId
+
+ForEach ($Dev in $Devs) {
+    Write-Host "Removing $($Dev.FriendlyName)" -ForegroundColor Cyan
+    $RemoveKey = "HKLM:\SYSTEM\CurrentControlSet\Enum\$($Dev.InstanceId)"
+    Get-Item $RemoveKey | Select-Object -ExpandProperty Property | %{ Remove-ItemProperty -Path $RemoveKey -Name $_ -Verbose }
+}
+Write-Host "Done.  Please restart!" -ForegroundColor Green
