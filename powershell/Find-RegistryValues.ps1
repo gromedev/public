d56d2634363aad2b3172946b4e39c1af58964f5e
gromedev <61883549+gromedev@users.noreply.github.com>
2024-01-18 16:10:16 +0100

    Update Find-RegistryValues.ps1

--- powershell/Find-RegistryValues.ps1
+++ powershell/Find-RegistryValues.ps1
@@ -1,3 +1,20 @@
+$RegPath = 'HKLM:\SOFTWARE\Microsoft\Windows Defender'
+$keyPattern = "*Dpa*"
+
+Get-ChildItem -Path $RegPath -Recurse | ForEach-Object {
+    $subkeyPath = $_.PSPath
+    $regKey = Get-ItemProperty -Path $subkeyPath -ErrorAction SilentlyContinue
+
+    $regKey.PSObject.Properties | ForEach-Object {
+        if ($_.Name -like $keyPattern) {
+            Write-Host "$subkeyPath\$($_.Name) = $($_.Value)"
+        }
+    }
+}
+
+# Microsoft.PowerShell.Core\Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Real-Time Protection\DpaDisabled = 0
+
+<# 
 $RegKey = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows Defender\Real-Time Protection')
 $key = "*Dpa*"
 
@@ -6,3 +23,4 @@ $RegKey.PSObject.Properties | ForEach-Object {
     Write-Host $_.Name ' = ' $_.Value
   }
 }
+#>
