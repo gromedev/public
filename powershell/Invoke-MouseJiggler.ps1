d5d5d56d63fb1ad726e6e46e137ce3df8dd9007a
gromedev <61883549+gromedev@users.noreply.github.com>
2024-07-30 13:30:52 +0200

    Update Invoke-MouseJiggler.ps1

--- powershell/Invoke-MouseJiggler.ps1
+++ powershell/Invoke-MouseJiggler.ps1
@@ -1,3 +1,39 @@
-#Add-Type -assemblyName System.Windows.Forms;$a=@(1..100);while(1){[System.Windows.Forms.Cursor]::Position=New-Object System.Drawing.Point(($a|get-random),($a|get-random));start-sleep -seconds 5}
+<#
+.DESCRIPTION 
+  Simulates physical mouse movements by using the SendInput function from the Windows API to create slight mouse movements every second.
+  This programmatically generated activity is designed to prevent the desktop from entering sleep mode by mimicking user interaction.
+#>
 
-Add-Type -AssemblyName System.Windows.Forms;while(1){[Windows.Forms.Cursor]::Position=(Get-Random 100);Start-Sleep 25}
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -MemberDefinition @"
+    [DllImport("user32.dll", EntryPoint = "SendInput", SetLastError = true)]
+    public static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+
+    public struct INPUT {
+        public int type;
+        public MOUSEINPUT mi;
+    }
+
+    public struct MOUSEINPUT {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    public const int INPUT_MOUSE = 0;
+    public const uint MOUSEEVENTF_MOVE = 0x0001;
+"@ -Name "User32" -Namespace "WinAPI"
+
+$inp = New-Object WinAPI.User32+INPUT
+$inp.type = [WinAPI.User32]::INPUT_MOUSE
+$inp.mi.dx = 1
+$inp.mi.dy = 1
+$inp.mi.dwFlags = [WinAPI.User32]::MOUSEEVENTF_MOVE
+
+while ($true) {
+    [WinAPI.User32]::SendInput(1, [ref]$inp, [System.Runtime.InteropServices.Marshal]::SizeOf($inp))
+    Start-Sleep -Seconds 1
+}
