992e93fedc3ed9079744850641a82e12a097a4ed
gromedev <61883549+gromedev@users.noreply.github.com>
2024-11-18 21:24:06 +0100

    Create New-gMSA.ps1

---
+++ powershell/New-gMSA.ps1
@@ -0,0 +1,12 @@
+New-ADServiceAccount -Name gMSA-Docker -DNSHostName srv-docker-01.i.cphtec.com -PrincipalsAllowedToRetrieveManagedPassword gMSA-Docker-Servers `
+-KerberosEncryptionType AES128, AES256
+
+Install-ADServiceAccount -Identity gMSA-Docker
+
+Test-ADServiceAccount gMSA-Docker
+
+New-CredentialSpec -AccountName gMSA-Docker
+
+# Typically found C:\ProgramData\docker\credentialspecs
+
+# docker run --security-opt "credentialspec=file://gMSA-Docker.json" -d your-container-image
