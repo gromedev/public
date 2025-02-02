326efd4a1a4355666da3dc6377d64291c5375a1c
gromedev <61883549+gromedev@users.noreply.github.com>
2025-02-03 00:47:45 +0100

    Create ADcheatsheet.ps1

---
+++ ADcheatsheet.ps1
@@ -0,0 +1,8 @@
+whoami /all
+systeminfo
+net user %username%
+net localgroup
+tasklist /svc
+icacls "C:\Program Files"
+schtasks /query /fo LIST
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall
